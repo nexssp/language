61c0963c866fd3a62f90de458f9298f1af311d4b
Marcin Polak <mapoart@gmail.com>
2021-05-21 17:57:53 +0200

    Removed relative nexssp/plugin and make it global @nexssp/plugin

--- bin/nexssp-language.js
+++ bin/nexssp-language.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const plugin = require('../nexssp_plugin')
+const plugin = require('@nexssp/plugin')
 // const _params = require('minimist')(process.argv.slice(2))
 const pluginRoot = plugin({ path: `${__dirname}/..`, ommit: ['format1'] })
 pluginRoot.start()
