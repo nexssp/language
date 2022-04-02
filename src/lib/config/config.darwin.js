3d1a7085b2f6e69aa1de1a2a68b871b15635e55d
Marcin Polak <mapoart@gmail.com>
2022-04-02 15:17:10 +0200

    fix for macos package manager.

--- src/lib/config/config.darwin.js
+++ src/lib/config/config.darwin.js
@@ -3,7 +3,10 @@ c.osPackageManagers = {
   brew: {
     installation:
       '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"',
-    installCommand: 'brew install'
-  }
+    installCommand: 'brew install',
+  },
 }
 c.compilerInstallation = 'brew install'
+
+c.dist = 'macOS'
+module.exports = c
